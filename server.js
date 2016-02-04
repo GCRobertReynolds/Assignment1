d1a0535f122f33b3b6bca297602fa387b75f4a30
RobReynolds <rob_reynolds_10@hotmail.com>
2016-02-03 23:48:50 -0500

    added routes for site structure

--- server.js
+++ server.js
@@ -5,43 +5,44 @@
  */
 /// <reference path="_reference.ts" />
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = process.env.port || 3000;
 //route for /main page
 app.get('/', function (req, res, next) {
-    res.send('Hello Express!');
+    res.sendFile(path.join(__dirname, "Public", "index.html"));
 });
 //route for /about page
 app.get('/about', function (req, res) {
-    res.send('About Page');
+    res.sendFile(path.join(__dirname, "Public", "about.html"));
 });
 //route for /projects page
 app.get('/projects', function (req, res) {
-    res.send('Projects Page');
+    res.sendFile(path.join(__dirname, "Public", "projects.html"));
 });
 //route for /services page
 app.get('/services', function (req, res) {
-    res.send('Services Page');
+    res.sendFile(path.join(__dirname, "Public", "services.html"));
 });
 //route for /contact page
 app.get('/contact', function (req, res) {
-    res.send('Contact Page');
+    res.sendFile(path.join(__dirname, "Public", "contact.html"));
 });
 //route for /interests page
 app.get('/interests', function (req, res) {
-    res.send('Interests Page');
+    res.sendFile(path.join(__dirname, "Public", "interests.html"));
 });
 //route for /programming page
 app.get('/programming', function (req, res) {
-    res.send('Programming Projects Page');
+    res.sendFile(path.join(__dirname, "Public", "programming.html"));
 });
 //route for /hobbies
 app.get('/hobbies', function (req, res) {
-    res.send('Hobby Projects Page');
+    res.sendFile(path.join(__dirname, "Public", "hobbies.html"));
 });
 //route for /resume
 app.get('/resume', function (req, res) {
-    res.send('Resume Page');
+    res.sendFile(path.join(__dirname, "Public", "resume.html"));
 });
 app.listen(port, function () {
     console.log("App Server Started.. On Port:" + port);
--- server.ts
+++ server.ts
@@ -7,47 +7,48 @@
 /// <reference path="_reference.ts" />
 
 import express = require('express');
+import path = require('path');
 
 var app:express.Express = express();
 var port:number = process.env.port || 3000;
 
 //route for /main page
 app.get('/', function(req:express.Request, res:express.Response, next:any) {
-   res.send('Hello Express!'); 
+   res.sendFile(path.join(__dirname, "Public", "index.html"));
 });
 
 //route for /about page
 app.get('/about', function(req:express.Request, res:express.Response) {
-    res.send('About Page');
+    res.sendFile(path.join(__dirname, "Public", "about.html"));
 });
 
 //route for /projects page
 app.get('/projects', function(req:express.Request, res:express.Response) {
-    res.send('Projects Page');
+    res.sendFile(path.join(__dirname, "Public", "projects.html"));
 });
 //route for /services page
 app.get('/services', function(req:express.Request, res:express.Response) {
-    res.send('Services Page');
+    res.sendFile(path.join(__dirname, "Public", "services.html"));
 });
 //route for /contact page
 app.get('/contact', function(req:express.Request, res:express.Response) {
-    res.send('Contact Page');
+    res.sendFile(path.join(__dirname, "Public", "contact.html"));
 });
 //route for /interests page
 app.get('/interests', function(req:express.Request, res:express.Response) {
-    res.send('Interests Page');
+    res.sendFile(path.join(__dirname, "Public", "interests.html"));
 });
 //route for /programming page
 app.get('/programming', function(req:express.Request, res:express.Response) {
-    res.send('Programming Projects Page');
+    res.sendFile(path.join(__dirname, "Public", "programming.html"));
 });
 //route for /hobbies
 app.get('/hobbies', function(req:express.Request, res:express.Response) {
-    res.send('Hobby Projects Page');
+    res.sendFile(path.join(__dirname, "Public", "hobbies.html"));
 });
 //route for /resume
 app.get('/resume', function(req:express.Request, res:express.Response) {
-    res.send('Resume Page');
+    res.sendFile(path.join(__dirname, "Public", "resume.html"));
 });
 
 app.listen(port, function(){
